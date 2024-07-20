d05929189bebb799a05a2879eb591a2c85b71235
Tae Yeong Yu <88639688+ramge132@users.noreply.github.com>
2024-07-20 23:43:50 +0900

    Update update_levels.py

--- update_levels.py
+++ update_levels.py
@@ -71,8 +71,12 @@ def update_readme():
         return False  # 테이블을 찾지 못하면 False 반환
 
     # 기존 테이블 부분을 대체할 새로운 테이블 생성
-    new_table = "<table>\n<tr>\n"
-    for user, file_count, user_level in user_data:
+    new_table = "<table>\n"
+    for i, (user, file_count, user_level) in enumerate(user_data):
+        if i % 7 == 0:
+            if i != 0:
+                new_table += "</tr>\n"  # 이전 행 닫기
+            new_table += "<tr>\n"  # 새로운 행 시작
         new_table += (
             f'  <td align="center">\n'
             f'    <a href="https://github.com/{user}">\n'
