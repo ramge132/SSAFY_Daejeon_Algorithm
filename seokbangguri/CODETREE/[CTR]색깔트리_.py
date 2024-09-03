6f8166b85d5b2e4a3bc4ebd4cfb82b0664cccfaf
seokbangguri <pique99@naver.com>
2024-09-03 23:29:00 +0900

    [CTR] / 색깔 트리 / PLATINUM 4 / 6H

--- seokbangguri/CODETREE/[CTR]색깔트리_.py
+++ seokbangguri/CODETREE/[CTR]색깔트리_.py
@@ -1,3 +1,13 @@
+def check_depth(p_id, depth = 2):
+    if p_id == -1:
+        return True
+
+    if nodes[p_id]['max_depth'] >= depth:
+        return check_depth(nodes[p_id]['p_id'], depth + 1)
+    
+    return False
+
+
 def update_colors(p_id, color):
     if p_id == -1:
         return
@@ -5,12 +15,10 @@ def update_colors(p_id, color):
     nodes[p_id]['colors'][color-1] = 1
     update_colors(nodes[p_id]['p_id'], color)
 
-    pass
-
 # 노드 추가
 def commandOne(code, m_id, p_id, color, max_depth):
     # 자식 노드가 추가 될 경우 부모노드의 max_depth보다 커지는 경우 추가 못함
-    if p_id != -1 and (len(nodes[p_id]['child_node_mids']) + 2) // 2 + 1 > nodes[p_id]['max_depth']:
+    if p_id != -1 and not check_depth(p_id):
         return
 
     # 자식 노드 생성
@@ -29,8 +37,6 @@ def commandOne(code, m_id, p_id, color, max_depth):
         nodes[p_id]['child_node_mids'].append(m_id)
         # 부모 노드가 가지고 있는 colors 업데이트
         update_colors(p_id, color)
-        # 부모 노드 current_depth 새로 계산
-        nodes[p_id]['current_depth'] = (len(nodes[p_id]['child_node_mids']) + 1) // 2 + 1
 
 def update_parent_colors(m_id):
     if m_id == -1:
@@ -50,7 +56,6 @@ def update_parent_colors(m_id):
                 temp_colors[i] = 1
 
     nodes[p_id]['colors'] = temp_colors
-    # update_parent_colors(nodes[p_id]['p_id'])
     update_parent_colors(p_id)
 
 # 색 변경
